97477d4afb5950f8d0719bcbed72e9058d2e29ea
Jacob Adams <jacob@tookmund.com>
2019-10-19 01:39:30 -0400

    get frame timings

---
+++ frametimes.py
@@ -0,0 +1,6 @@
+convertToTimestamp(fps, imgnum):
+    framenum = (imgnum-1)*100
+    secondNum = frameNum // fps
+    mins = secondNum // 60
+    seconds = secondNum % 60
+    return mins+":"+seconds
