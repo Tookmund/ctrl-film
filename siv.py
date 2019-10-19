c3640038cecddf0e51c5bb6e65662f059fd15a42
Jacob Adams <jacob@tookmund.com>
2019-10-19 12:33:44 -0400

    siv: begin audio but comment it

--- siv.py
+++ siv.py
@@ -10,31 +10,40 @@ import io
 import s3
 import ocr
 import frametimes
+import transcribe
+import hash
 
-def v2json(video):
+def v2json(video, h=None):
     pwd = os.getcwd()+'/'
     with tempfile.TemporaryDirectory() as td:
         os.chdir(td)
         if video.startswith('http'):
             filename = subprocess.run(['youtube-dl', '--get-filename', video], text=True, capture_output=True).stdout[:-1]
+            if h is None:
+                h = hash.hash(video)
         else:
-            filename = video
-        screen = filename+".screen"
-        fobj = s3.download(screen)
-        if not fobj:
+            filename=video
+            if h is None:
+                h = hash.hash(open(filename))
+        screen = h+".screen"
+        sobj = s3.download(screen)
+        #audio = filename+".audio"
+        #aobj = s3.download(audio)
+        if not sobj:
             if video.startswith('http'):
                 ytdl = subprocess.run(["youtube-dl", video], check=True)
-            else:
-                ext = os.path.splitext(video)[1]
-                os.replace(pwd+video, video)
-        else:
-            return fobj.read()
-        fps = subprocess.run([pwd+"getimages.sh", filename], check=True, capture_output=True, text=True).stdout.split('\n')[0]
-        d = ocr.img2text(td, fps)
-        j = json.dumps(d)
-        s = io.BytesIO(j.encode())
-        s3.upload(s, screen)
-        return j
+            fps = subprocess.run([pwd+"getimages.sh", filename], check=True, capture_output=True, text=True).stdout.split('\n')[0]
+            sd = ocr.img2text(td, fps)
+            jv = json.dumps(d)
+            s = io.BytesIO(jv.encode())
+            s3.upload(s, screen)
+            sobj = sd
+            #ad = transcribe(td+filename)
+            #ja = json.dumps(d)
+            #a = io.BytesIO(jv.encode())
+            #s3.upload(a, audio)
+            #aobj = a
+        return sobj
 
 if __name__ == '__main__':
     print(v2json(sys.argv[1]))
