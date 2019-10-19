e1a8ab6a58c05d1d8dc280dbdddd3513b55a9e30
Dante12129 <dan9coasterdan@gmail.com>
2019-10-19 11:54:13 -0400

    Move Functionality From Video Route To Function

--- app.py
+++ app.py
@@ -1,19 +1,16 @@
-from flask import Flask
+from flask import Flask, jsonify
 import tempfile
 import os
 import forms
 import siv
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
 app.secret_key=os.environ.get("SECRET_KEY") or os.urandom(16)
 
-@app.route('/')
-def index():
-    return 'Hello World!\n'
 
-@app.route('/video/', methods=['GET', 'POST'])
-def video():
+def formToJSON():
     form = forms.upload()
     if form.validate_on_submit():
         with tempfile.TemporaryDirectory() as td:
@@ -23,4 +20,14 @@ def video():
             j = siv.v2json(file_path)
             return j
     else:
-        return 'no form yet\n'
+        return jsonify({'message', 'No form sent.'}), 400
+
+
+@app.route('/')
+def index():
+    return 'Hello World!\n'
+
+
+@app.route('/video/', methods=['GET', 'POST'])
+def video():
+    return formToJSON()
