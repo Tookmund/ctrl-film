94a73be622e2cbe327ce75b89068e2c3381d782a
Jacob Adams <jacob@tookmund.com>
2019-10-19 09:34:13 -0400

    ocr: Return sorted and ordered dictionary based on timestamp

--- ocr.py
+++ ocr.py
@@ -2,13 +2,17 @@
 import os
 import sys
 import pytesseract
+from collections import OrderedDict
+from frametimes import convertToTimestamp
 
-def img2text(folder):
-    s = ''
-    for pic in os.listdir(folder):
+def img2text(folder, fps):
+    d = OrderedDict()
+    for pic in sorted(os.listdir(folder)):
         if pic.endswith('.png'):
+            t = pic[4:-4]
             ts = convertToTimestamp(float(fps), int(t))
-    return s
+            d[ts] = pytesseract.image_to_string(folder+'/'+pic)
+    return d
 
 if __name__ == '__main__':
     img2text(sys.argv[1])
