da81d9bb54b9cdbee84a8f016b4ac585c9bbd8fc
Jacob Adams <jacob@tookmund.com>
2019-10-19 11:11:02 -0400

    Upload to s3

--- siv.py
+++ siv.py
@@ -5,31 +5,37 @@ import subprocess
 import os
 import tempfile
 import json
+import io
 
+import s3
 import ocr
 import frametimes
 
-def v2dict(video):
+def v2json(video):
     pwd = os.getcwd()+'/'
     with tempfile.TemporaryDirectory() as td:
         os.chdir(td)
         if video.startswith('http'):
-            ytdl = subprocess.run(["youtube-dl", "-ovideo", sys.argv[1]], check=True)
+            filename = subprocess.run(['youtube-dl', '--get-filename', video], text=True, capture_output=True).stdout[:-1]
         else:
-            ext = os.path.splitext(video)[1]
-            os.replace(pwd+video, "video"+ext)
-        videofile = [fn for fn in os.listdir(td) if fn.startswith("video")][0]
+            filename = video
+        screen = filename+".screen"
+        fobj = s3.download(screen)
+        if not fobj:
+            if video.startswith('http'):
+                ytdl = subprocess.run(["youtube-dl", video], check=True)
+            else:
+                ext = os.path.splitext(video)[1]
+                os.replace(pwd+video, video)
+        else:
+            return fobj.read()
+        videofile = [fn for fn in os.listdir(td)][0]
         fps = subprocess.run([pwd+"getimages.sh", videofile], check=True, capture_output=True, text=True).stdout.split('\n')[0]
-        return ocr.img2text(td, fps)
-
-def v2json(video):
-    return json.dumps(v2dict(video))
-
-def search(video, string):
-    d = v2dict(video)
-    for k,v in d.items():
-        if string in v:
-            print(k, ':', v)
+        d = ocr.img2text(td, fps)
+        j = json.dumps(d)
+        s = io.BytesIO(j.encode())
+        s3.upload(s, screen)
+        return j
 
 if __name__ == '__main__':
-    search(sys.argv[1], sys.argv[2])
+    print(v2json(sys.argv[1]))
