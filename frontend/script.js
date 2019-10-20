9905f945a031fe4ef9bf40c88cad25c3d4c722f1
Jacob Adams <jacob@tookmund.com>
2019-10-19 20:33:28 -0400

    querySelector not selection

--- frontend/script.js
+++ frontend/script.js
@@ -33,7 +33,7 @@ const app = new Vue({
 			this.statusMessage = "Loading...";
 			this.isSearching = true;
 			formData = new FormData();
-			file = document.querySelection('input[type=file]').files[0];
+			file = document.querySelector('input[type=file]').files[0];
 			formData.append('file', file);
 			fetch("http://search-in-video.tookmund.com/video", {  
 				method: 'POST',
